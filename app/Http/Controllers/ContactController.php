33cc51eff69ea751be801ca704da936cdff49246
vtorres96 <victorcdc96@gmail.com>
2019-11-08 19:10:04 -0300

    added functions: show, store and update

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Contact; 
-use App\User; 
+use App\User;
 
 class ContactController extends Controller
 {
@@ -16,11 +16,49 @@ class ContactController extends Controller
         return view('contact.index')->with('contacts', $contacts);
     }
 
+    public function show($id){
+        $contact = Contact::find($id);
+
+        return view('contact.show')->with('contact', $contact);
+    }
+
     public function create(){
         return view('contact.create');
     }
 
-    public function store(){
+    public function store(Request $request){
+        $request->validate([
+            'nome' => 'required|max:50',
+            'sobrenome' => 'required|max:50',                    
+            'telefone' => 'required|max:15',
+            'email' => 'email|max:80|unique:contacts',
+            'data_de_nascimento' => 'required',
+            'avatar' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+        ]);
+
+        $arquivo = $request->file('avatar');
+
+        if (empty($arquivo)) {
+            $caminhoRelativo = 'null';
+        } else {
+            $arquivo->storePublicly('uploads');
+            $caminhoAbsoluto = public_path()."/storage/uploads";
+            $nomeArquivo = $arquivo->getClientOriginalName();
+            $caminhoRelativo = "storage/uploads/$nomeArquivo";
+            $arquivo->move($caminhoAbsoluto, $nomeArquivo);
+        }
+
+        $contact = Contact::create([
+            'first_name' => $request->input('nome'),
+            'last_name' => $request->input('sobrenome'),
+            'phone_number' => $request->input('telefone'),
+            'email' => $request->input('email'),
+            'date_of_birth' => $request->input('data_de_nascimento'),
+            'avatar' => $caminhoRelativo,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return redirect('/contacts');
     }
 
     public function edit($id){
@@ -29,7 +67,36 @@ class ContactController extends Controller
         return view('contact.edit')->with('contact', $contact);
     }
 
-    public function update(){
+    public function update(Request $request, $id){
+        $request->validate([
+            'nome' => 'required|max:50',
+            'sobrenome' => 'required|max:50',                    
+            'telefone' => 'required|max:15',
+            'data_de_nascimento' => 'required',
+            'avatar' => 'nullable|sometimes|image|mimes:jpg,jpeg,png,gif'
+        ]);
+
+        $arquivo = $request->file('avatar');
+
+        if (empty($arquivo)) {
+            $caminhoRelativo = 'null';
+        } else {
+            $arquivo->storePublicly('uploads');
+            $caminhoAbsoluto = public_path()."/storage/uploads";
+            $nomeArquivo = $arquivo->getClientOriginalName();
+            $caminhoRelativo = "storage/uploads/$nomeArquivo";
+            $arquivo->move($caminhoAbsoluto, $nomeArquivo);
+        }
+
+        $contact = Contact::update([
+            'first_name' => $request->input('nome'),
+            'last_name' => $request->input('sobrenome'),
+            'phone_number' => $request->input('telefone'),
+            'date_of_birth' => $request->input('data_de_nascimento'),
+            'avatar' => $caminhoRelativo
+        ]);
+
+        return redirect('/contacts');
     }
 
     public function destroy($id){
