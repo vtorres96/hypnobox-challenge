1755927eb70b277b56380ccea2388de06dde0fb7
vtorres96 <victorcdc96@gmail.com>
2019-11-08 19:07:55 -0300

    added function getAvatarImageAttribute

--- app/Contact.php
+++ app/Contact.php
@@ -9,6 +9,10 @@ class Contact extends Model
     protected $table = "contacts";
     protected $primaryKey = "id";
     protected $fillable = [
-        "first_name", "last_name", "phone_number", "email", "date_of_birth", "avatar"
+        "first_name", "last_name", "phone_number", "email", "date_of_birth", "avatar", "user_id"
     ];
+
+    public function getAvatarImageAttribute($value) {
+        return $this->avatar == 'null' ? asset('img/null.png') : asset($this->avatar);
+    }
 }
