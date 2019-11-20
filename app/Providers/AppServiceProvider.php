20b059dee506eb2ef89d89b751f9f485a72e8c57
vtorres96 <victorcdc96@gmail.com>
2019-11-20 15:16:33 -0300

    added image preview

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLenth(191);
+        Schema::defaultStringLength(191);
     }
 }
--- resources/views/contact/edit.blade.php
+++ resources/views/contact/edit.blade.php
@@ -15,26 +15,30 @@
                             {{ method_field('PUT') }}
 
                             <div class="form-group">
+                                <img src="{{ $contact->avatar_image }}" class="rounded-circle" width="230" height="230">
+                            </div>
+
+                            <div class="form-group">
                                 <label for="nome">Nome</label>
-                                <input type="text" class="form-control{{$errors->has('nome') ? ' is-invalid':''}}" value="{{ old('nome',  $contact->first_name) }}" id="nome" name="nome">
+                                <input type="text" class="form-control{{$errors->has('nome') ? ' is-invalid':''}}" value="{{ $contact->first_name }}" id="nome" name="nome">
                                 <div class="invalid-feedback">{{ $errors->first('nome') }}</div>
                             </div>
 
                             <div class="form-group">
                                 <label for="sobrenome">Sobrenome</label>
-                                <input type="text" class="form-control{{$errors->has('sobrenome') ? ' is-invalid':''}}" value="{{ old('sobrenome', $contact->last_name) }}" id="sobrenome" name="sobrenome">
+                                <input type="text" class="form-control{{$errors->has('sobrenome') ? ' is-invalid':''}}" value="{{ $contact->last_name }}" id="sobrenome" name="sobrenome">
                                 <div class="invalid-feedback">{{ $errors->first('sobrenome') }}</div>
                             </div>
 
                             <div class="form-group">
                                 <label for="telefone">Telefone</label>
-                                <input type="text" class="form-control{{$errors->has('telefone') ? ' is-invalid':''}}" value="{{ old('telefone', $contact->phone_number) }}" id="telefone" name="telefone">
+                                <input type="text" class="form-control{{$errors->has('telefone') ? ' is-invalid':''}}" value="{{ $contact->phone_number }}" id="telefone" name="telefone">
                                 <div class="invalid-feedback">{{ $errors->first('telefone') }}</div>
                             </div>
 
                             <div class="form-group">
                                 <label for="email">E-mail</label>
-                                <input type="email" class="form-control{{$errors->has('email') ? ' is-invalid':''}}" value="{{ old('email', $contact->email) }}" id="email" name="email" placeholder="email@example.com.br">
+                                <input type="email" class="form-control{{$errors->has('email') ? ' is-invalid':''}}" value="{{ $contact->email }}" id="email" name="email" placeholder="email@example.com.br">
                                 <div class="invalid-feedback">{{ $errors->first('email') }}</div>
                             </div>
 
