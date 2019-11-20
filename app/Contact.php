3e0ab48675ad616fb18734eb68cd13e1e9ac2dfd
vtorres96 <victorcdc96@gmail.com>
2019-11-20 11:22:58 -0300

    added rellationship user x contacts

--- app/Contact.php
+++ app/Contact.php
@@ -9,8 +9,12 @@ class Contact extends Model
     protected $table = "contacts";
     protected $primaryKey = "id";
     protected $fillable = [
-        "first_name", "last_name", "phone_number", "email", "date_of_birth", "avatar", "user_id"
+        "first_name", "last_name", "phone_number", "email", "avatar", "user_id"
     ];
+
+    public function user(){
+        return $this->hasOne(User::class, "id", "user_id");
+    }
 
     public function getAvatarImageAttribute($value) {
         return $this->avatar == null ? asset('img/null.png') : asset($this->avatar);
